88fcd8ec68d53db82e7cdeffe4aa5742c95ab015
Nkululeko Sibeko <112828662+nkululekosibeko@users.noreply.github.com>
2024-08-31 14:25:12 +0200

    Initial Commit

---
+++ HotelWaveFinal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HotelWaveFinal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
