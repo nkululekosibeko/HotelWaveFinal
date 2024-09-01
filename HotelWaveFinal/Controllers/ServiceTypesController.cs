b1971f3439a084e8dc3cccece6a620cdf1af7104
Nkululeko Sibeko <112828662+nkululekosibeko@users.noreply.github.com>
2024-09-01 03:39:39 +0200

    Scafolder the not so efficient entities and made UI designs for them.

---
+++ HotelWaveFinal/Controllers/ServiceTypesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HotelWaveFinal.DB;
+using HotelWaveFinal.Models;
+
+namespace HotelWaveFinal.Controllers
+{
+    public class ServiceTypesController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+
+        public ServiceTypesController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ServiceTypes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ServiceTypes.ToListAsync());
+        }
+
+        // GET: ServiceTypes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var serviceType = await _context.ServiceTypes
+                .FirstOrDefaultAsync(m => m.ServiceTypeId == id);
+            if (serviceType == null)
+            {
+                return NotFound();
+            }
+
+            return View(serviceType);
+        }
+
+        // GET: ServiceTypes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ServiceTypes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ServiceTypeId,TypeName,Description,BasePrice")] ServiceType serviceType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(serviceType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(serviceType);
+        }
+
+        // GET: ServiceTypes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var serviceType = await _context.ServiceTypes.FindAsync(id);
+            if (serviceType == null)
+            {
+                return NotFound();
+            }
+            return View(serviceType);
+        }
+
+        // POST: ServiceTypes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ServiceTypeId,TypeName,Description,BasePrice")] ServiceType serviceType)
+        {
+            if (id != serviceType.ServiceTypeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(serviceType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ServiceTypeExists(serviceType.ServiceTypeId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(serviceType);
+        }
+
+        // GET: ServiceTypes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var serviceType = await _context.ServiceTypes
+                .FirstOrDefaultAsync(m => m.ServiceTypeId == id);
+            if (serviceType == null)
+            {
+                return NotFound();
+            }
+
+            return View(serviceType);
+        }
+
+        // POST: ServiceTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var serviceType = await _context.ServiceTypes.FindAsync(id);
+            if (serviceType != null)
+            {
+                _context.ServiceTypes.Remove(serviceType);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ServiceTypeExists(int id)
+        {
+            return _context.ServiceTypes.Any(e => e.ServiceTypeId == id);
+        }
+    }
+}
---
+++ HotelWaveFinal/Controllers/ServicesController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HotelWaveFinal.DB;
+using HotelWaveFinal.Models;
+
+namespace HotelWaveFinal.Controllers
+{
+    public class ServicesController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+
+        public ServicesController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Services
+        public async Task<IActionResult> Index()
+        {
+            var applicationDBContext = _context.Services.Include(s => s.ServiceType);
+            return View(await applicationDBContext.ToListAsync());
+        }
+
+        // GET: Services/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var service = await _context.Services
+                .Include(s => s.ServiceType)
+                .FirstOrDefaultAsync(m => m.ServiceId == id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            return View(service);
+        }
+
+        // GET: Services/Create
+        public IActionResult Create()
+        {
+            ViewData["ServiceTypeId"] = new SelectList(_context.ServiceTypes, "ServiceTypeId", "ServiceTypeId");
+            return View();
+        }
+
+        // POST: Services/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ServiceId,ServiceName,Description,TotalCost,ServiceTypeId")] Service service)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(service);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ServiceTypeId"] = new SelectList(_context.ServiceTypes, "ServiceTypeId", "ServiceTypeId", service.ServiceTypeId);
+            return View(service);
+        }
+
+        // GET: Services/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var service = await _context.Services.FindAsync(id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+            ViewData["ServiceTypeId"] = new SelectList(_context.ServiceTypes, "ServiceTypeId", "ServiceTypeId", service.ServiceTypeId);
+            return View(service);
+        }
+
+        // POST: Services/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ServiceId,ServiceName,Description,TotalCost,ServiceTypeId")] Service service)
+        {
+            if (id != service.ServiceId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(service);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ServiceExists(service.ServiceId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ServiceTypeId"] = new SelectList(_context.ServiceTypes, "ServiceTypeId", "ServiceTypeId", service.ServiceTypeId);
+            return View(service);
+        }
+
+        // GET: Services/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var service = await _context.Services
+                .Include(s => s.ServiceType)
+                .FirstOrDefaultAsync(m => m.ServiceId == id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            return View(service);
+        }
+
+        // POST: Services/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var service = await _context.Services.FindAsync(id);
+            if (service != null)
+            {
+                _context.Services.Remove(service);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ServiceExists(int id)
+        {
+            return _context.Services.Any(e => e.ServiceId == id);
+        }
+    }
+}
